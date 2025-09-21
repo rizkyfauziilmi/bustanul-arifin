2fe3d48b8a146e3aea8b386f12737937e5cb7278
Rizky Fauzi Ilmi <rizkyfauziilmi@gmail.com>
2025-09-21 19:50:47 +0700

    Add padding to hero section heading on mobile

--- src/components/hero/hero-section.tsx
+++ src/components/hero/hero-section.tsx
@@ -13,7 +13,7 @@ export const HeroSection = () => {
         variant="gradient"
       />
       <DarkOverlay className="md:hidden block" darkness={0.7} variant="solid" />
-      <h1 className="text-white relative md:absolute md:left-18 md:bottom-24 text-5xl text-center md:text-left md:text-6xl md:h-fit h-full flex items-center justify-center">
+      <h1 className="text-white p-4 md:p-0 relative md:absolute md:left-18 md:bottom-24 text-5xl text-center md:text-left md:text-6xl md:h-fit h-full flex items-center justify-center">
         Lorem ipsum dolor sit amet.
         <br className="md:block hidden" />
         Lorem, ipsum dolor.
